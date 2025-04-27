aefe0caee6f6862e13def46a6c61d63cc26e4579
moath62how <moath26how@gmail.com>
2025-04-27 15:53:06 +0300

    added BasePage and Reg

--- src/main/java/BasePage.java
+++ src/main/java/BasePage.java
@@ -1,2 +1,12 @@
+import org.openqa.selenium.*;
+
 public class BasePage {
+	protected final WebDriver driver;
+
+	public BasePage(WebDriver driver) {
+		this.driver = driver;
+	}
+	public WebElement find(By locator){
+		return driver.findElement(locator);
+	}
 }
--- src/main/java/RegisterPage.java
+++ src/main/java/RegisterPage.java
@@ -1,2 +1,27 @@
-public class RegisterPage {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class RegisterPage extends BasePage {
+	By usernameLocator = By.id("username");
+	By passwordLocator = By.id("password");
+	By confirmPasswordLocator = By.id("confirmPassword");
+	By emailLocator = By.id("email");
+	By submitBtnLocator = By.className("btn-primary");
+
+	WebElement usernameField = find(usernameLocator);
+	WebElement passwordField = find(passwordLocator);
+	WebElement confirmPasswordField = find(confirmPasswordLocator);
+	WebElement emailField = find(emailLocator);
+	WebElement submitBtn = find(submitBtnLocator);
+	public RegisterPage(WebDriver driver){
+		super(driver);
+	}
+	public void register(String username,String password,String confirmPassword,String email){
+			usernameField.sendKeys(username);
+			passwordField.sendKeys(password);
+			confirmPasswordField.sendKeys(confirmPassword);
+			emailField.sendKeys(email);
+			submitBtn.click();
+	}
 }
